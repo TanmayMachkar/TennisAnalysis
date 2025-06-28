3334064a706bd68e8e7d7a3e9c9eed66ce169580
TanmayMachkar <tanmaymacho@gmail.com>
2025-06-28 23:24:30 +0530

    done with court will start ball interpolation tmrw

--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -11,7 +11,7 @@ from utils import (
 class MiniCourt:
 	def __init__(self, frame):
 		self.drawing_rectangle_width = 250
-		self.drawing_rectangle_height = 450
+		self.drawing_rectangle_height = 500
 		self.buffer = 50
 		self.padding_court = 20
 
@@ -101,6 +101,25 @@ class MiniCourt:
 		self.start_x = self.end_x - self.drawing_rectangle_width
 		self.start_y = self.end_y - self.drawing_rectangle_height
 
+	def draw_court(self, frame):
+		for i in range(0, len(self.draw_key_points), 2):
+			x = int(self.draw_key_points[i])
+			y = int(self.draw_key_points[i + 1])
+			cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+
+		# draw lines
+		for line in self.lines:
+			start_point = (int(self.draw_key_points[line[0] * 2]), int(self.draw_key_points[line[0] * 2 + 1]))
+			end_point = (int(self.draw_key_points[line[1] * 2]), int(self.draw_key_points[line[1] * 2 + 1]))
+			cv2.line(frame, start_point, end_point, (0, 0, 0), 2)
+
+		# draw net
+		net_start_point = (self.draw_key_points[0], int((self.draw_key_points[1] + self.draw_key_points[5]) / 2))
+		net_end_point = (self.draw_key_points[2], int((self.draw_key_points[1] + self.draw_key_points[5]) / 2))
+		cv2.line(frame, net_start_point, net_end_point, (255, 0, 0), 2)
+
+		return frame
+
 	# transparent white background for court
 	def draw_background_rectangle(self, frame):
 		shapes = np.zeros_like(frame, np.uint8)
@@ -117,6 +136,16 @@ class MiniCourt:
 		output_frames = []
 		for frame in frames:
 			frame = self.draw_background_rectangle(frame)
+			self.draw_court(frame)
 			output_frames.append(frame)
 
 		return output_frames
+
+	def get_start_point_of_mini_court(self):
+		return (self.court_start_x, self.court_start_y)
+
+	def get_width_of_mini_court(self):
+		return self.court_drawing_width
+
+	def get_court_drawing_points(self):
+		return self.draw_key_points
