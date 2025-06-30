1e2dfaba8a3a570581657f92a74328ad711cdf7d
TanmayMachkar <tanmaymacho@gmail.com>
2025-06-30 14:12:20 +0530

    done with project

--- trackers/ball_tracker.py
+++ trackers/ball_tracker.py
@@ -20,6 +20,39 @@ class BallTracker:
 
 		return ball_positions
 
+	def get_ball_shot_frames(self, ball_positions):
+		ball_positions = [x.get(1, []) for x in ball_positions] # the list will be empty when there are no detections
+		df_ball_positions = pd.DataFrame(ball_positions, columns = ['x1', 'y1', 'x2', 'y2'])
+
+		df_ball_positions['ball_hit'] = 0
+
+		# get center coordinate of ball
+		df_ball_positions['mid_y'] = (df_ball_positions['y1'] + df_ball_positions['y2']) / 2
+		df_ball_positions['mid_y_rolling_mean'] = df_ball_positions['mid_y'].rolling(window = 5, min_periods = 1, center = False).mean() # remove outliers
+		# identify when the y changes
+		df_ball_positions['delta_y'] = df_ball_positions['mid_y_rolling_mean'].diff() # calculates diff btwn consecutive rows
+		minimum_change_frames_for_hit = 25 # approx number of frames for which there was no abrupt change in ball direction
+		for i in range(1, len(df_ball_positions) - int(minimum_change_frames_for_hit * 1.2)): # 1.2 = increase by 20%
+		    negative_position_change = df_ball_positions['delta_y'].iloc[i] > 0 and df_ball_positions['delta_y'].iloc[i+1] < 0 # first increasing and then decreasing
+		    positive_position_change = df_ball_positions['delta_y'].iloc[i] < 0 and df_ball_positions['delta_y'].iloc[i+1] > 0 # first decreasing and then increasing
+
+		    if negative_position_change or positive_position_change:
+		        change_count = 0 # keeps track of how many frames the change goes on for
+		        for change_frame in range(i + 1, i + int(minimum_change_frames_for_hit * 1.2) + 1):
+		            negative_position_change_following_frame = df_ball_positions['delta_y'].iloc[i] > 0 and df_ball_positions['delta_y'].iloc[change_frame] < 0 # compare original frame with future frames
+		            positive_position_change_following_frame = df_ball_positions['delta_y'].iloc[i] < 0 and df_ball_positions['delta_y'].iloc[change_frame] > 0
+
+		            if negative_position_change and negative_position_change_following_frame:
+		                change_count += 1
+		            elif positive_position_change and positive_position_change_following_frame:
+		                change_count += 1
+
+		        if change_count>minimum_change_frames_for_hit-1:
+		            df_ball_positions['ball_hit'].iloc[i] = 1 # If enough frames (≥ 25) show the direction has changed and stayed changed, it flags the frame i as a ball hit.
+		frame_nums_with_ball_hits = df_ball_positions[df_ball_positions['ball_hit'] == 1].index.tolist()
+
+		return frame_nums_with_ball_hits
+
 	# detect multiple frames
 	def detect_frames(self, frames, read_from_stub = False, stub_path = None): # read_from_stub is used to avoid running model again and again, if True just read stored model from stub_path
 		ball_detections = []
